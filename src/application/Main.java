53dd8c0fc8eb98c0e9b1e67397384a3cce2600ca
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-04-04 15:24:03 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,22 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
