dd1b41ca72a5939887d0d04c2b36df918dac6354
Lucas William <59783795+LucasJordao@users.noreply.github.com>
2020-04-09 14:34:13 -0300

    Adding databse access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DepartmentDao;
+import model.dao.FactoryDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = FactoryDao.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Electronics"));
-		
+		List<Department> list = dao.findAll();
 		return list;
 	}
 }
